49fc3602beb4e0e056841cc519bfbb1360bdfc5b
Gerardo J Garcia <gerardojgarcia@pm.me>
2024-09-04 12:42:45 -0700

    first commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue";
+
+const localTime = ref("");
+let timer;
+
+function updateLocalTime() {
+    const now = new Date();
+    localTime.value = now.toLocaleTimeString();
+}
+
+onMounted(() => {
+    // Update time immediately
+    updateLocalTime();
+
+    // Update time every second
+    timer = setInterval(updateLocalTime, 1000);
+});
+
+onUnmounted(() => {
+    // Clear the interval when the component is unmounted
+    if (timer) {
+        clearInterval(timer);
+    }
+});
+</script>
+<template>
+    <header class="flex justify-between w-full font-bold px-4">
+        <strong>headlessflower</strong>
+        <nav class="w-1/3 px-4 flex justify-evenly">
+            <NuxtLink to="/">Work</NuxtLink>
+            <NuxtLink to="/">Services </NuxtLink>
+            <NuxtLink to="/">Agency</NuxtLink>
+            <NuxtLink to="/">Contact</NuxtLink>
+        </nav>
+        <p>-{{ localTime }}-</p>
+        <NuxtLink class="border-black border-b-2">Get a proposal</NuxtLink>
+    </header>
+</template>
---
+++ components/TheHero.vue
@@ -0,0 +1,40 @@
+<script setup></script>
+
+<template>
+    <div>
+        <TheHeader />
+        <div class="hero-grid grid grid-rows-2">
+            <div class="grid grid-cols-2 place-items-center">
+                <div class="w-1/2">
+                    <h1 class="text-4xl">
+                        We transform visions into masterpieces
+                        <strong class="italic">for pioneering brands</strong>
+                    </h1>
+                    <div></div>
+                </div>
+                <div class="grid grid-cols-1 place-items-center p-16">
+                    <p class="w-3/4 text-2xl">
+                        trusted by brands for exceptional design solutions
+                    </p>
+                    <div class="tech-stack text-4xl my-8 space-x-8">
+                        <NuxtLink to="/">
+                            <Icon name="ri:blender-line p-0" />
+                        </NuxtLink>
+                        <Icon
+                            name="ri:settings-2-line"
+                            class="rounded-xl border-2 border-gray-300 shadow-sm"
+                        />
+                        <Icon
+                            name="ri:inbox-fill"
+                            class="rounded-xl border-2 border-gray-300 shadow-sm p-4"
+                        />
+                        <Icon
+                            name="ri:blender-line"
+                            class="rounded-xl border-2 border-gray-300 shadow-sm p-4"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
