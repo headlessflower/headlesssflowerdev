0decabcda12d4cdb04b6c40c634c4dd6a966677d
Gerardo Garcia <gerardogarcia@Jennifers-MacBook-Air.local>
2025-08-06 06:12:22 -0400

    new york updates

--- components/FeaturesSection.vue
+++ components/FeaturesSection.vue
@@ -1,34 +1,38 @@
-<script setup>
-</script>
+<script setup></script>
 <template>
-    <div class="grid place-items-center pb-16">
+    <div class="grid place-items-center py-16">
         <strong class="text-4xl mb-16">Best in the business</strong>
 
         <div class="feature-container grid md:grid-cols-3 p-8 gap-6">
             <div class="grid space-y-4">
                 <Icon name="ri:computer-line" class="text-3xl" />
                 <strong class="text-3xl">Expertise and Specialization</strong>
-              <ul class="space-y-2">
-
-                <li>Our team of experts specializes in web design, development,
-                  SEO, and digital marketing</li>
-                <li>Ensuring high-quality, custom
-                  solutions.</li>
-                <li> We stay ahead of industry trends to deliver
-                  creative and effective outcomes for your unique business
-                  needs.</li>
-              </ul>
-
+                <ul class="space-y-2">
+                    <li>
+                        Our team of experts specializes in web design,
+                        development, SEO, and digital marketing
+                    </li>
+                    <li>Ensuring high-quality, custom solutions.</li>
+                    <li>
+                        We stay ahead of industry trends to deliver creative and
+                        effective outcomes for your unique business needs.
+                    </li>
+                </ul>
             </div>
 
             <div class="grid space-y-4">
                 <Icon name="ri:timer-flash-line" class="text-3xl" />
                 <strong class="text-3xl">Cost & Time Efficiency </strong>
                 <ul class="space-y-2">
-                  <li>We streamline the development process with advanced tools</li>
+                    <li>
+                        We streamline the development process with advanced
+                        tools
+                    </li>
                     <li>saving you time and minimizing expenses.</li>
-                  <li>Our approach prevents costly mistakes</li>
-                  <li> Focus on your core business while we handle the details</li>
+                    <li>Our approach prevents costly mistakes</li>
+                    <li>
+                        Focus on your core business while we handle the details
+                    </li>
                 </ul>
             </div>
             <div class="grid space-y-4">
@@ -36,15 +40,11 @@
                 <strong class="text-3xl"
                     >Long-Term Support & Scalability</strong
                 >
-                <ul  class="space-y-2">
-                  <li>
-                    Ongoing maintenance and updates
-                  </li>
-                  <li>Your digital presence evolves with your business. </li>
-                  <li>scalable
-                    solutions grow with you</li>
-                  <li>Providing long-term support for
-                    future challenges.</li>
+                <ul class="space-y-2">
+                    <li>Ongoing maintenance and updates</li>
+                    <li>Your digital presence evolves with your business.</li>
+                    <li>scalable solutions grow with you</li>
+                    <li>Providing long-term support for future challenges.</li>
                 </ul>
             </div>
         </div>
